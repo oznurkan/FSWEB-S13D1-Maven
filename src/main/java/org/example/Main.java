5eda344e722cf063bf407fc3cfed59d3b8243e3c
oznurkan <oznurkan35@gmail.com>
2026-02-16 22:11:44 +0300

    upload project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,28 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        return isBarking
+                && clock >= 0
+                && clock <= 23
+                && (clock >= 20 || clock < 8);
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return ( firstAge >= 13 && firstAge <= 19)
+                || ( secondAge >= 13 && secondAge <= 19)
+                || ( thirdAge >= 13 && thirdAge <= 19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        return ( isSummer && ( temp >= 25 && temp<= 45))
+                || ( !isSummer && ( temp >= 25 && temp <= 35));
     }
 
     public static double area(double width, double height) {
-       
+       return ( width < 0 || height < 0 ? -1 : width* height);
     }
 
     public static double area(double radius) {
-       
+       return ( radius < 0 ? -1 : radius * radius * Math.PI);
     }
 }
